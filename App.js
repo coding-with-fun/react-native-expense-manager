49a2389d8919211f4f7dafdf73f6d8596558e236
Coderc <harsh@coderc.in>
2021-04-18 09:59:28 +0530

    Added Sign In Form

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import SignInScreen from "./screens/SignInScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <PaperProvider>
+            <StatusBar style="auto" />
+            <SignInScreen />
+        </PaperProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
